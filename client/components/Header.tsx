55219c6a3a818b54934be600f6cb869854ed9fbf
Gurwinder Singh <gurwinder8186@gmail.com>
2024-11-17 03:09:24 +1300

    add header component

---
+++ client/components/Header.tsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom'
+
+function Header() {
+  
+  return(
+  <header>
+    <h1> SINGH&apos;S TECH</h1>
+    <div>
+      <input type="text" placeholder='Search' />
+      <button> 🔍</button>
+    </div>
+    <div>
+      <Link to="/shop">Shop Now</Link>
+      <Link to="/register">Register</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/cart">Cart</Link>
+    </div>
+  </header>
+
+)}
+export default Header
