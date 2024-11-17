efc0f9fbc18f04e6709032eeade5bd74d5f371b2
Gurwinder Singh <gurwinder8186@gmail.com>
2024-11-17 18:08:26 +1300

    add component to app for rendring and linking css to index.html

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,4 +1,3 @@
-// import { useFruits } from '../hooks/useFruits.ts'
 import { BrowserRouter } from 'react-router-dom'
 import Header from './Header.tsx'
 import Sidebar from './Sidebar.tsx'
@@ -11,8 +10,6 @@ function App() {
         <div className="app">
         <Header />
         <Sidebar />
-        {/* <h1>Fullstack Boilerplate - with Fruits!</h1>
-        <ul>{data && data.map((fruit) => <li key={fruit}>{fruit}</li>)}</ul> */}
         <Footer />
       </div>
     </BrowserRouter>
