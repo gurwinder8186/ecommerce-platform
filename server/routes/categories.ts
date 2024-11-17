02c65456defd6925589a2ba952252ca59ccb4906
Gurwinder Singh <gurwinder8186@gmail.com>
2024-11-17 18:05:03 +1300

    add database funciton

---
+++ server/routes/categories.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express'
+
+import * as db from '../db/categories.ts'
+
+const router = Router()
+
+router.get('/', async (req, res) => {
+  try {
+    const categories = await db.getAllCategories()
+
+    res.json(categories)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: 'Something went wrong' })
+  }
+})
+
+export default router
