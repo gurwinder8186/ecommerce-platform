897235afe523c070cf9b418c651dc210e63f57b8
Gurwinder Singh <gurwinder8186@gmail.com>
2024-11-19 09:48:45 +1300

    add desktoppc interface

--- models/Category.ts
+++ models/Category.ts
@@ -3,3 +3,23 @@ export interface Category {
   name: string;
   description?:string;
 }
+export interface DesktopPC {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  imageUrl: string;
+  specifications: {
+    processor: string;
+    ram: string;
+    storage: string;
+    gpu?: string;
+    os: string;
+    [key: string]: string;
+  };
+  stock: number;
+  brand: string;
+  model: string;
+  warranty: string;
+  category_id: number;
+}
